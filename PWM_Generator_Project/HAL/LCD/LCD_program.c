e56527f722fc03ed5a60cbe1b103869f7aafc0bc
mohamedhammad224 <153463442+mohamedhammad224@users.noreply.github.com>
2023-12-26 22:09:12 +0200

    Update LCD

--- PWM_Generator_Project/HAL/LCD/LCD_program.c
+++ PWM_Generator_Project/HAL/LCD/LCD_program.c
@@ -11,19 +11,42 @@ static void GLCD_WriteCommand(u8 command) {
     DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_RS, LOW);     // Make RS pin LOW for command register
     DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_RW, LOW);     // Make RW pin LOW for write operation
     DIO_Write_Port_val(GLCD_DATA_PORT, command);
-    DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_EN, HIGH);    // Make EN pin HIGH for enable signal
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D4, CHECK_BIT(command, 4));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D5, CHECK_BIT(command, 5));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D6, CHECK_BIT(command, 6));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D7, CHECK_BIT(command, 7));
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, HIGH);          // Make EN pin HIGH for enable signal
     _delay_ms(2);
-    DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_EN, LOW);     // Make EN pin LOW for enable signal
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, LOW);           // Make EN pin LOW for enable signal
+    _delay_ms(2);
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D4, CHECK_BIT(command, 0));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D5, CHECK_BIT(command, 1));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D6, CHECK_BIT(command, 2));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D7, CHECK_BIT(command, 3));
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, HIGH);          // Make EN pin HIGH for enable signal
+    _delay_ms(2);
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, LOW);           // Make EN pin LOW for enable signal
     _delay_ms(2);
 }
 
 static void GLCD_WriteData(u8 data) {
     DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_RS, HIGH);     // Make RS pin HIGH for command register
     DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_RW, LOW);     // Make RW pin LOW for write operation
-    DIO_Write_Port_val(GLCD_DATA_PORT, data);
-    DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_EN, HIGH);    // Make EN pin HIGH for enable signal
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D4, CHECK_BIT(data, 4));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D5, CHECK_BIT(data, 5));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D6, CHECK_BIT(data, 6));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D7, CHECK_BIT(data, 7));
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, HIGH);          // Make EN pin HIGH for enable signal
     _delay_ms(2);
-    DIO_Wirte_Pin_val(GLCD_COMMAND_PORT, GLCD_EN, LOW);     // Make EN pin LOW for enable signal
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, LOW);           // Make EN pin LOW for enable signal
+    _delay_ms(2);
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D4, CHECK_BIT(data, 0));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D5, CHECK_BIT(data, 1));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D6, CHECK_BIT(data, 2));
+    DIO_SetPinVal(LCD_DATA_PORT, LCD_D7, CHECK_BIT(data, 3));
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, HIGH);          // Make EN pin HIGH for enable signal
+    _delay_ms(2);
+    DIO_SetPinVal(LCD_CONTROL_PORT, LCD_EN, LOW);           // Make EN pin LOW for enable signal
     _delay_ms(2);
 }
 
